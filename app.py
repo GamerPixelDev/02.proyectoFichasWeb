c757451bdbf85a1c0fcb991c036e18683e5f8c9c
GamerPixelDev <agamerpixel@gmail.com>
2025-10-31 13:40:34 +0100

    varios cambios sin importancia

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port = port)
+    #Comentar arriba para hacer un .exe con waitress
+    #Descomentar abajo para hacer un .exe con waitress
+    #from waitress import serve
+    #serve(app, host="127.0.0.1", port=port)
